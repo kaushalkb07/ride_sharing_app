883ad5e5ddb0cf63237763baf591bef7b4265f99
kaushalkb07 <iamkaushalkb@gmail.com>
2025-06-29 20:22:43 +0545

    Font Added: Poppins

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Ride Sharing UI',
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        fontFamily: 'Poppins', // Set global font to Poppins
+        scaffoldBackgroundColor: const Color(0xFFF5F6F7),
+        primarySwatch: Colors.indigo,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          iconTheme: IconThemeData(color: Colors.black),
+          titleTextStyle: TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(fontSize: 14),
+          bodyLarge: TextStyle(fontSize: 16),
+        ),
+      ),
       home: HomePage(),
     );
   }
